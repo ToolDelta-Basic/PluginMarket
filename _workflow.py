780f82239fb2d0be094043a914dffc53bca70b46
SuperScript-PRC <2528622340@qq.com>
2024-04-13 22:50:11 +0800

    连 market_tree.json 也使用 plugin-id 了！

--- _workflow.py
+++ _workflow.py
@@ -39,12 +39,14 @@ def get_latest_versions(directory):
 def flush_basic_datas():
     with open("market_tree.json", "r", encoding="utf-8") as f:
         market_d = json.load(f)
+    market_d["MarketPlugins"] = {}
     for path in os.listdir():
         datpath = os.path.join(path, "datas.json")
         if os.path.isfile(datpath):
             with open(datpath, "r", encoding="utf-8") as f:
                 dat = json.load(f)
-                market_d["MarketPlugins"][path] = {
+                market_d["MarketPlugins"][dat["plugin-id"]] = {
+                    "name": path,
                     "author": dat["author"],
                     "version": dat["version"],
                     "plugin-type": dat["plugin-type"]
