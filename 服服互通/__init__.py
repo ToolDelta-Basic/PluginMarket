0e5e69b21609ed8138a9a2884e862263ec9e4b26
SuperScript-PRC <2528622340@qq.com>
2024-08-14 13:30:21 +0800

    更新插件: 服服互通 (域名更换)

--- 服服互通/__init__.py
+++ 服服互通/__init__.py
@@ -1,4 +1,5 @@
 import websockets
+import requests
 import json
 import time
 import asyncio
@@ -6,14 +7,12 @@ import base64
 import threading
 from socket import gaierror
 from dataclasses import dataclass
-from typing import Any
-from collections.abc import Callable
+from typing import Any, Callable
 from websockets.exceptions import ConnectionClosed
 from websockets.legacy.client import WebSocketClientProtocol
 from tooldelta import Frame, plugins, Plugin, Config, Print, Utils
 
-# CUSTOMIZE CLASSES AND FUNCS
-
+# CUSTOMIZE CLASSES AND FUNCS 自定义的类和方法
 
 @dataclass
 class Data:
@@ -22,18 +21,18 @@ class Data:
     content: dict
 
     def marshal(self) -> str:
-        return json.dumps({"Type": self.type, "Content": self.content})
-
+        return json.dumps({
+            "Type": self.type,
+            "Content": self.content
+        })
 
 def format_data(type: str, content: dict):
     return Data(type, content)
-
 
 def new_result_getter():
     lock = threading.Lock()
     lock.acquire()
     resp: list[None | Data] = [None]
-
     def setter(result: Data):
         resp[0] = result
         lock.release()
@@ -44,9 +43,7 @@ def new_result_getter():
 
     return setter, getter
 
-
-# PROTOCOL CLASSES
-
+# PROTOCOL CLASSES 服服互通协议类
 
 class BasicProtocol:
     # 所有服服互通协议的基类
@@ -72,7 +69,6 @@ class BasicProtocol:
     def listen_for_data(self, data_type: str, cb: Callable[[Any], None]):
         raise NotImplementedError
 
-
 class SuperLinkProtocol(BasicProtocol):
     def __init__(self, frame: Frame, ws_ip: str, cfgs: dict):
         super().__init__(frame, ws_ip, cfgs)
@@ -83,6 +79,9 @@ class SuperLinkProtocol(BasicProtocol):
 
     @Utils.thread_func("服服互通自动重连线程")
     def start(self):
+        if self.ws_ip == "自动选择线路":
+            name, self.ws_ip = self.find_default_ips()
+            Print.print_suc(f"服服互通: 已自动选择线路 {name}")
         while 1:
             asyncio.run(self.start_ws_con())
             self.retryCount += 1
@@ -93,33 +92,34 @@ class SuperLinkProtocol(BasicProtocol):
             Print.print_war(f"服服互通断开连接, 将在 {self.retryTime}s 后重连")
             time.sleep(self.retryTime)
 
+    def find_default_ips(self):
+        Print.print_war("服服互通: 正在自动获取线路中..")
+        try:
+            resp = requests.get("https://tdload.whiteleaf.cn/raw.githubusercontent.com/ToolDelta-Basic/SuperLink/main/source.json")
+            resp.raise_for_status()
+            default_name, default_ip = list(resp.json().items())[0]
+            return default_name, default_ip
+        except Exception as err:
+            Print.print_err(f"服服互通: 无法自动获取线路: {err}; 此插件已禁用")
+            raise SystemExit
+
     async def start_ws_con(self):
         try:
-            async with websockets.connect(  # type: ignore
+            async with websockets.connect( # type: ignore
                 self.ws_ip,
-                extra_headers={
+                extra_headers = {
                     "Protocol": "SuperLink-v4@SuperScript",
-                    "ServerName": base64.b64encode(
-                        self.cfgs["此租赁服的公开显示名"].encode("utf-8")
-                    ).decode("ascii"),
-                    "ChannelName": base64.b64encode(
-                        self.cfgs["登入后自动连接到的频道大区名"].encode("utf-8")
-                    ).decode("ascii"),
-                    "ChannelToken": base64.b64encode(
-                        self.cfgs["频道密码"].encode("utf-8")
-                    ).decode("ascii"),
-                },
+                    "ServerName": base64.b64encode(self.cfgs["此租赁服的公开显示名"].encode("utf-8")).decode("ascii"),
+                    "ChannelName": base64.b64encode(self.cfgs["登入后自动连接到的频道大区名"].encode("utf-8")).decode("ascii"),
+                    "ChannelToken": base64.b64encode(self.cfgs["频道密码"].encode("utf-8")).decode("ascii"),
+                }
             ) as ws:
                 self.ws: WebSocketClientProtocol = ws
                 self.active = True
                 login_resp_json = json.loads(await ws.recv())
-                login_resp = format_data(
-                    login_resp_json["Type"], login_resp_json["Content"]
-                )
+                login_resp = format_data(login_resp_json["Type"], login_resp_json["Content"])
                 if login_resp.type == "server.auth_failed":
-                    Print.print_err(
-                        f"§b服服互通: 中心服务器登录失败: {login_resp.content['Reason']}"
-                    )
+                    Print.print_err(f"§b服服互通: 中心服务器登录失败: {login_resp.content['Reason']}")
                 elif login_resp.type == "server.auth_success":
                     Print.print_suc("服服互通: §f中心服务器登录成功")
                     self.retryCount = 0
@@ -127,16 +127,11 @@ class SuperLinkProtocol(BasicProtocol):
                         await self.handle(json.loads(await ws.recv()))
 
         except ConnectionClosed:
-            Print.print_war("服服互通: 服务器断开连接")
+            Print.print_war(f"服服互通: 服务器断开连接")
         except gaierror as err:
-            Print.print_err(
-                f"服服互通: 中心服务器连接失败(IP解析异常): {self.ws_ip} - {err}"
-            )
+            Print.print_err(f"服服互通: 中心服务器连接失败(IP解析异常): {self.ws_ip} - {err}")
         except Exception as err:
-            import traceback
-
-            traceback.print_exc()
-            Print.print_err(f"服服互通: 中心服务器连接失败: {err}")
+            Print.print_err(f"服服互通: 中心服务器 ({self.ws_ip}) 连接失败: {err}")
         finally:
             self.active = False
 
@@ -154,7 +149,7 @@ class SuperLinkProtocol(BasicProtocol):
     def format_data(type: str, content: dict):
         return format_data(type, content)
 
-    async def send_and_wait_req(self, data: Data, timeout=-1):
+    async def send_and_wait_req(self, data: Data, timeout = -1):
         await self.send(data)
         req_id = data.content["UUID"]
         s, g = new_result_getter()
@@ -165,15 +160,13 @@ class SuperLinkProtocol(BasicProtocol):
     def listen_for_data(self, data_type: str, cb: Callable[[Data], None]):
         self.listen_cbs[data_type] = cb
 
-
 # PLUGIN MAIN
-
 
 @plugins.add_plugin_as_api("服服互通")
 class SuperLink(Plugin):
     name = "服服互通v4"
     author = "SuperScript"
-    version = (0, 0, 6)
+    version = (0, 0, 7)
 
     def __init__(self, frame: Frame):
         super().__init__(frame)
@@ -182,17 +175,17 @@ class SuperLink(Plugin):
 
     def read_cfgs(self):
         CFG_DEFAULT = {
-            "中心服务器IP": "ws://superlink.tblstudio.cn:24013",
+            "中心服务器IP": "自动选择线路",
             "服服互通协议": "SuperLink-v4@SuperScript",
             "协议附加配置": {
                 "此租赁服的公开显示名": "???",
                 "登入后自动连接到的频道大区名": "公共大区",
-                "频道密码": "",
+                "频道密码": ""
             },
             "基本互通配置": {
                 "是否转发玩家发言": True,
-                "转发聊天到本服格式": "<[服名]:[玩家名]> [消息]",
-            },
+                "转发聊天到本服格式": "<[服名]:[玩家名]> [消息]"
+            }
         }
         CFG_STD = {
             "中心服务器IP": str,
@@ -200,9 +193,12 @@ class SuperLink(Plugin):
             "协议附加配置": {
                 "此租赁服的公开显示名": str,
                 "登入后自动连接到的频道大区名": str,
-                "频道密码": str,
+                "频道密码": str
             },
-            "基本互通配置": {"是否转发玩家发言": bool, "转发聊天到本服格式": str},
+            "基本互通配置": {
+                "是否转发玩家发言": bool,
+                "转发聊天到本服格式": str
+            }
         }
         self.cfg, _ = Config.get_plugin_config_and_version(
             self.name, CFG_STD, CFG_DEFAULT, self.version
@@ -213,9 +209,7 @@ class SuperLink(Plugin):
         if use_protocol is None:
             Print.print_err(f"协议不受支持: {self.cfg['服服互通协议']}")
             raise SystemExit
-        self.active_protocol = use_protocol(
-            self.frame, self.cfg["中心服务器IP"], self.cfg["协议附加配置"]
-        )
+        self.active_protocol = use_protocol(self.frame, self.cfg["中心服务器IP"], self.cfg["协议附加配置"])
         basic_link_cfg = self.cfg["基本互通配置"]
         self.enable_trans_chat = basic_link_cfg["是否转发玩家发言"]
         self.trans_fmt = basic_link_cfg["转发聊天到本服格式"]
@@ -237,22 +231,18 @@ class SuperLink(Plugin):
     @Utils.thread_func
     def on_player_message(self, player, msg):
         if self.enable_trans_chat and self.active:
-            asyncio.run(
-                self.send(format_data("chat.msg", {"ChatName": player, "Msg": msg}))
-            )
+            asyncio.run(self.send(format_data(
+                "chat.msg", {
+                    "ChatName": player,
+                    "Msg": msg
+                }
+            )))
 
     @plugins.add_broadcast_listener("superlink.event")
     def listen_chat(self, data: Data):
         if data.type == "chat.msg" and self.enable_trans_chat:
-            self.game_ctrl.say_to(
-                "@a",
-                Utils.simple_fmt(
-                    {
-                        "[服名]": data.content["Sender"],
-                        "[玩家名]": data.content["ChatName"],
-                        "[消息]": data.content["Msg"],
-                    },
-                    self.cfg["基本互通配置"]["转发聊天到本服格式"],
-                ),
-            )
+            self.game_ctrl.say_to("@a", Utils.simple_fmt(
+                {"[服名]": data.content["Sender"], "[玩家名]": data.content["ChatName"], "[消息]": data.content["Msg"]},
+                self.cfg["基本互通配置"]["转发聊天到本服格式"]
+            ))
         return True
