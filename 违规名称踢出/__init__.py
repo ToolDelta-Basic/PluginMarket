5f9e966fbbf0ccf4ebe0a35820e4b8b6269b7ff6
daqyt <git@dqyt.online>
2024-10-22 20:30:31 +0800

    new file:   "\350\277\235\350\247\204\345\220\215\347\247\260\350\270\242\345\207\272/__init__.py" 	new file:   "\350\277\235\350\247\204\345\220\215\347\247\260\350\270\242\345\207\272/datas.json"

---
+++ 违规名称踢出/__init__.py
@@ -0,0 +1,36 @@
+from tooldelta import Frame, Plugin, Config, Print, Builtins, plugins, Utils, game_utils
+import os
+import time
+import random
+@plugins.add_plugin
+class kill(Plugin):
+    version = (0, 0, 1)
+    name = "违规名称踢出"
+    author = "大庆油田"
+    description = "简单的违规名称踢出"
+    
+    def __init__(self,frame: Frame):
+      self.frame = frame
+      self.game_ctrl = frame.get_game_control()
+      self.make_data_path()
+      CFG_DEFAULT = {
+        "踢出词":[],
+        "原因":""
+      }
+      self.cfg, _ = Config.get_plugin_config_and_version(
+             self.name, {}, CFG_DEFAULT, self.version
+        )
+      self.ci= self.cfg["踢出词"]
+      self.yy= self.cfg["原因"]
+
+    @Utils.thread_func
+    def killpl(self,player: str):
+       for a in self.ci:
+          if a in player:
+             self.game_ctrl.sendcmd_with_resp(f"/kick {player} {self.yy}")
+   
+    def on_player_message(self, player: str):
+      self.killpl(player)
+
+    def on_player_join(self, player: str):
+      self.killpl(player)
