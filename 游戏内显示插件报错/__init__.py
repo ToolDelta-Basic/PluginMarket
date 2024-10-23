677ee76f25e92eb9b08ca4f90f2f9ad0c185a8b3
SuperScript-PRC <2528622340@qq.com>
2024-10-23 16:54:26 +0800

    游戏内显示插件报错: fix game_ctrl 未找到 报错

--- 游戏内显示插件报错/__init__.py
+++ 游戏内显示插件报错/__init__.py
@@ -6,12 +6,23 @@ GN = "\n"
 class InGameError(Plugin):
     name = "游戏内显示插件报错"
     author = "System"
-    version = (0, 0, 1)
+    version = (0, 0, 2)
 
     def __init__(self, frame: ToolDelta):
+        super().__init__(frame)
         self.frame = frame
         self.frame.on_plugin_err = self.call_err
+        self.frame.add_console_cmd_trigger(["error", "报错"], "[报错文本]", "测试游戏内报错显示", self.test_error)
 
     def call_err(self, plugin_name: str, exception: Exception, exc_str: str):
         self.game_ctrl.say_to("@a", f"§7[§cERROR§7] §4{plugin_name} 报错：")
         self.game_ctrl.say_to("@a", f" §c{f'{GN} '.join(exc_str.split(GN))}")
+
+    def test_error(self, args):
+        if len(args) == 0:
+            return
+        self.game_ctrl.sendcmd(f"w @s tooldelta.error.test {args[0]}")
+
+    def on_player_message(self, player: str, msg: str):
+        if player == self.game_ctrl.bot_name and msg.startswith("tooldelta.error.test "):
+            raise AssertionError(msg.removeprefix("tooldelta.error.test "))
