5f276a2abb3b0f462de876bc180dfddbc3ec2afd
SuperScript-PRC <2528622340@qq.com>
2025-01-28 22:41:09 +0800

    Removed due to some critical situations

--- bdx导入器/BDXConverter/Operation/SubtractXValue.py
+++
@@ -1,8 +0,0 @@
-from ..General.GeneralClass import GeneralClass
-
-
-class SubtractXValue(GeneralClass):
-    def __init__(self) -> None:
-        super().__init__()
-        self.operationName: str = 'SubtractXValue'
-        self.operationNumber: int = 15
