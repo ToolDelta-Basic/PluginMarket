a1c49a4e1aa4f8e6ddba01650ff96e96472e8d5a
SuperScript-PRC <2528622340@qq.com>
2025-02-04 09:49:14 +0800

    更新插件 违规名称踢出

--- 违规名称踢出/__init__.py
+++ 违规名称踢出/__init__.py
@@ -1,12 +1,9 @@
 from tooldelta import Frame, Plugin, Config, Print, Builtins, plugins, Utils, game_utils
-import os
-import time
-import random
 
 
 @plugins.add_plugin
 class kill(Plugin):
-    version = (0, 0, 1)
+    version = (0, 0, 3)
     name = "违规名称踢出"
     author = "大庆油田"
     description = "简单的违规名称踢出"
@@ -26,7 +23,15 @@ class kill(Plugin):
     def killpl(self, player: str):
         for a in self.ci:
             if a in player:
-                self.game_ctrl.sendcmd_with_resp(f"/kick {player} {self.yy}")
+                self.game_ctrl.sendcmd_with_resp(f'/kick "{player}" {self.yy}')
+
+    @Utils.thread_func("踢出敏感词玩家")
+    def mgc_test(self, player: str):
+        try:
+            self.game_ctrl.sendcmd(f'/w errcmd "{player}"', True, timeout=4)
+        except TimeoutError:
+            Print.print_war(f"玩家 {player} 名字为敏感词, 已经踢出")
+            self.game_ctrl.sendcmd_with_resp(f'/kick "{player}" {self.yy}')
 
     def on_player_message(self, player: str, _):
         self.killpl(player)
