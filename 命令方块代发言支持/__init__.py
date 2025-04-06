86dd60b04b49ae3b208ac51e0e57761671166c47
SuperScript-PRC <2528622340@qq.com>
2025-04-06 19:54:28 +0800

    上传插件 命令方块代发言支持

---
+++ 命令方块代发言支持/__init__.py
@@ -0,0 +1,26 @@
+from tooldelta import Plugin, plugin_entry, Chat, constants
+from tooldelta.utils import packet_transition
+
+class NewPlugin(Plugin):
+    name = "命令方块代发言支持"
+    author = "SuperScript"
+    version = (0, 0, 1)
+
+    def __init__(self, frame):
+        super().__init__(frame)
+        self.ListenPacket(constants.PacketIDS.Text, self.on_chat_pk)
+        self.ListenChat(lambda chat: print("Chat", chat.msg))
+
+    def on_chat_pk(self, pk: dict):
+        sender, msg, verified = packet_transition.get_playername_and_msg_from_text_packet(self.frame, pk)
+        if sender is None or msg is None or sender not in self.game_ctrl.allplayers:
+            return False
+        player = self.frame.get_players().getPlayerByName(sender)
+        if player is None:
+            return False
+        if not verified:
+            self.frame.plugin_group.execute_chat(Chat(player, msg), self.frame.on_plugin_err)
+        return False
+
+
+entry = plugin_entry(NewPlugin)
