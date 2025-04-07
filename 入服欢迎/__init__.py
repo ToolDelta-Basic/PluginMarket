ddaea6796723d356e2e1e9788f3d068c70aa44f2
SuperScript-PRC <2528622340@qq.com>
2025-04-07 20:57:19 +0800

    将此插件重写为类式插件

--- 入服欢迎/__init__.py
+++ 入服欢迎/__init__.py
@@ -1,48 +1,46 @@
-import asyncio
-from tooldelta.plugin_load.injected_plugin import (
-    player_join,
-    player_left,
-    player_name,
-)
-from tooldelta.game_utils import sendwscmd
-from tooldelta import cfg as config
-
-__plugin_meta__ = {
-    "name": "入服欢迎",
-    "version": "0.0.3",
-    "author": "wling",
-}
+import time
+from tooldelta import ToolDelta, cfg as config, utils, Player, Plugin, plugin_entry
 
 
-STD_BAN_CFG = {"登出时发送指令": list, "登录时发送指令": list, "登录时延迟发送": int}
-DEFAULT_BAN_CFG: dict[str, list[str] | int] = {
-    "登出时发送指令": [
-        """/tellraw @a {\"rawtext\":[{\"text\":\"§a§lBye~ @[target_player]\"}]}"""
-    ],
-    "登录时发送指令": [
-        """/tellraw [target_player] {\"rawtext\":[{\"text\":\"§a您可以使用在聊天栏发送 §b.help §a以调出系统面板§f.\"}]}"""
-    ],
-    "登录时延迟发送": 10,
-}
+class JoinWelcome(Plugin):
+    name = "入服欢迎"
+    author = "wling"
+    version = (0, 0, 4)
 
-cfg, cfg_version = config.get_plugin_config_and_version(
-    __plugin_meta__["name"],
-    STD_BAN_CFG,
-    DEFAULT_BAN_CFG,
-    __plugin_meta__["version"].split("."),
-)
+    def __init__(self, frame: ToolDelta):
+        super().__init__(frame)
+        STD_BAN_CFG = {
+            "登出时发送指令": list,
+            "登录时发送指令": list,
+            "登录时延迟发送": int,
+        }
+        DEFAULT_BAN_CFG: dict[str, list[str] | int] = {
+            "登出时发送指令": [
+                """/tellraw @a {\"rawtext\":[{\"text\":\"§a§lBye~ @[target_player]\"}]}"""
+            ],
+            "登录时发送指令": [
+                """/tellraw [target_player] {\"rawtext\":[{\"text\":\"§a您可以使用在聊天栏发送 §b.help §a以调出系统面板§f.\"}]}"""
+            ],
+            "登录时延迟发送": 10,
+        }
+        self.cfg, _ = config.get_plugin_config_and_version(
+            self.name,
+            STD_BAN_CFG,
+            DEFAULT_BAN_CFG,
+            self.version,
+        )
+        self.ListenPlayerJoin(self.on_player_join)
+        self.ListenPlayerLeave(self.on_player_leave)
+
+    @utils.thread_func("入服欢迎")
+    def on_player_join(self, player: Player) -> None:
+        time.sleep(self.cfg["登录时延迟发送"])
+        for i in self.cfg["登录时发送指令"]:
+            self.game_ctrl.sendwocmd(i.replace("[target_player]", player.name))
+
+    def on_player_leave(self, player: Player) -> None:
+        for i in self.cfg["登出时发送指令"]:
+            self.game_ctrl.sendwocmd(i.replace("[target_player]", player.name))
 
 
-@player_join()
-async def _(playerNameData: player_name) -> None:
-    player = playerNameData.playername
-    await asyncio.sleep(10)
-    for i in cfg["登录时发送指令"]:
-        sendwscmd(i.replace("[target_player]", player))
-
-
-@player_left()
-async def _(playerNameData: player_name) -> None:
-    player = playerNameData.playername
-    for i in cfg["登出时发送指令"]:
-        sendwscmd(i.replace("[target_player]", player))
+entry = plugin_entry(JoinWelcome)
