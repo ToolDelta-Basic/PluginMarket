254e0332e15046cde766265cdab336a757c071be
SuperScript-PRC <2528622340@qq.com>
2025-04-07 20:56:37 +0800

    将此插件重写为类式插件

--- @玩家/__init__.py
+++ @玩家/__init__.py
@@ -1,17 +1,4 @@
-from tooldelta.plugin_load.injected_plugin import player_message, player_message_info
-from tooldelta.game_utils import (
-    rawText,
-    sendcmd,
-    get_all_player,
-)
-
-
-__plugin_meta__ = {
-    "name": "@玩家",
-    "version": "0.0.4",
-    "description": "当有人提及你时，会收到提醒",
-    "author": "wling",
-}
+from tooldelta import Plugin, Chat, ToolDelta, plugin_entry
 
 display = "§a§l@提醒 §7>>> §r"
 
@@ -20,19 +7,31 @@ def find_mentions(text, player_list):
     return [player for player in player_list if f"@{player}" in text]
 
 
-@player_message()
-async def _(playerinfo: player_message_info):
-    message = playerinfo.message
-    playername = playerinfo.playername
-    # 如果文字包含@
-    if "@" in message:
-        mentioned_players = find_mentions(message, get_all_player())
-        for i in mentioned_players:
-            rawText(i, display + "§l§a有人提及了你！")
-            sendcmd(f"""/title {i} title §b§l有人提及了你""")
-            sendcmd(f'''/title {i} subtitle "§7{playername} > §e§l{message}"''')
-            sendcmd(
-                r"""/execute """
-                + i
-                + """ ~ ~ ~ playsound block.bell.hit @s ~ ~ ~ 1 1 1"""
-            )
+class AtPlayer(Plugin):
+    name = "@玩家"
+    author = "wling"
+    version = (0, 0, 5)
+    description = "当有人提及你时，会收到提醒"
+
+    def __init__(self, frame: ToolDelta):
+        super().__init__(frame)
+        self.ListenChat(self.on_chat)
+
+    def on_chat(self, chat: Chat):
+        message = chat.msg
+        playername = chat.player.name
+        # 如果文字包含@
+        if "@" in message:
+            mentioned_players = find_mentions(message, self.game_ctrl.allplayers)
+            for i in mentioned_players:
+                self.game_ctrl.say_to(i, display + "§l§a有人提及了你！")
+                self.game_ctrl.player_title(i, "§b§l有人提及了你")
+                self.game_ctrl.player_subtitle(i, f"§7{playername} > §e§l{message}")
+                self.game_ctrl.sendcmd(
+                    r"""/execute """
+                    + i
+                    + """ ~ ~ ~ playsound block.bell.hit @s ~ ~ ~ 1 1 1"""
+                )
+
+
+entry = plugin_entry(AtPlayer)
