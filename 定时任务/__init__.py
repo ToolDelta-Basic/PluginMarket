d27fc2eceee57d232d680b8e5fc56f53ada71235
SuperScript-PRC <2528622340@qq.com>
2025-04-07 20:57:53 +0800

    将此插件重写为类式插件

--- 定时任务/__init__.py
+++ 定时任务/__init__.py
@@ -1,11 +1,8 @@
-import asyncio
 import os
 import shutil
 import time
 
-from tooldelta import Print, utils, constants
-from tooldelta.plugin_load.injected_plugin import init
-from tooldelta.game_utils import sendwocmd
+from tooldelta import ToolDelta, constants, fmts, utils, Plugin, plugin_entry
 
 __plugin_meta__ = {
     "name": "定时任务",
@@ -25,18 +22,6 @@ data_path = os.path.join(constants.TOOLDELTA_PLUGIN_DATA_DIR, "定时任务")
 tasks: dict[int, list[Task]] = {}
 
 
-@init()
-async def on_inject():
-    if not os.path.isdir(data_path):
-        os.makedirs(data_path)
-        c_dir = os.path.join(os.path.dirname(__file__), "示例定时任务")
-        for file in os.listdir(c_dir):
-            shutil.copy(os.path.join(c_dir, file), data_path)
-        Print.print_suc("定时任务: 已创建示例文件")
-    read_files()
-    utils.createThread(asyncio.run, (run_tasks(),), "计划任务主线程")
-
-
 def read_files():
     for file in os.listdir(data_path):
         if file.endswith(".mcfunction"):
@@ -50,7 +35,7 @@ def load_mcf_file(path: str):
     try:
         s_time, is_spec = parse_sth(content)
     except SyntaxError as err:
-        Print.print_err(f"无法加载计划任务 {name}: {err}")
+        fmts.print_err(f"无法加载计划任务 {name}: {err}")
         return
     cmds = []
     for ln in content.split("\n"):
@@ -59,7 +44,7 @@ def load_mcf_file(path: str):
     if s_time not in tasks.keys():
         tasks[s_time] = []
     tasks[s_time].append(Task(cmds, s_time, is_spec))
-    Print.print_suc(f"计划任务: 已加载 {s_time}s任务 {name}")
+    fmts.print_suc(f"计划任务: 已加载 {s_time}s任务 {name}")
 
 
 def parse_sth(f: str):
@@ -77,28 +62,50 @@ def parse_sth(f: str):
     return s_time, spec
 
 
-async def run_tasks():
-    counter = 0
-    while 1:
-        await asyncio.sleep(1)
-        for k, v in tasks.items():
-            if counter % k == 0:
-                await asyncio.gather(*(execute_task(task) for task in v))
-        counter += 1
+class ScheduledTasks(Plugin):
+    name = "定时任务"
+    author = "System"
+    version = (0, 0, 5)
 
+    def __init__(self, frame: ToolDelta):
+        super().__init__(frame)
+        if not os.path.isdir(data_path):
+            os.makedirs(data_path)
+            c_dir = os.path.join(os.path.dirname(__file__), "示例定时任务")
+            for file in os.listdir(c_dir):
+                shutil.copy(os.path.join(c_dir, file), data_path)
+            fmts.print_suc("定时任务: 已创建示例文件")
+        read_files()
+        self.ListenActive(self.on_inject)
 
-async def execute_task(task: Task):
-    if task.spec:
-        execute_task_spec(task)
-    else:
-        for cmd in task.cmds:
-            sendwocmd(cmd)
+    def on_inject(self):
+        self.run_tasks()
 
+    @utils.thread_func("计划任务主线程")
+    def run_tasks(self):
+        counter = 0
+        while 1:
+            time.sleep(1)
+            for k, v in tasks.items():
+                if counter % k == 0:
+                    for task in v:
+                        self.execute_task(task)
+            counter += 1
 
-@utils.thread_func("特殊计划任务执行")
-def execute_task_spec(task: Task):
-    for cmd in task.cmds:
-        if cmd.startswith("sleep"):
-            time.sleep(utils.try_int(cmd[6:]) or 0)
+    def execute_task(self, task: Task):
+        if task.spec:
+            self.execute_task_spec(task)
         else:
-            sendwocmd(cmd)
+            for cmd in task.cmds:
+                self.game_ctrl.sendwocmd(cmd)
+
+    @utils.thread_func("特殊计划任务执行")
+    def execute_task_spec(self, task: Task):
+        for cmd in task.cmds:
+            if cmd.startswith("sleep"):
+                time.sleep(utils.try_int(cmd[6:]) or 0)
+            else:
+                self.game_ctrl.sendwocmd(cmd)
+
+
+entry = plugin_entry(ScheduledTasks)
