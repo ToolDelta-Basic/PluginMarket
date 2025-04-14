7a3d7d83ddd9aa661a6d47db83075245b3b2bc42
SuperScript-PRC <2528622340@qq.com>
2025-04-15 00:31:10 +0800

    全服喇叭: 规范化聊天栏菜单

--- 全服喇叭/__init__.py
+++ 全服喇叭/__init__.py
@@ -1,37 +1,36 @@
-from tooldelta import Plugin, ToolDelta, plugin_entry
+from tooldelta import Player, Plugin, ToolDelta, plugin_entry
 
 
 class 全服喇叭(Plugin):
     name = "全服喇叭"
     author = "wling"
-    version = (0, 0, 5)
+    version = (0, 0, 6)
 
     def __init__(self, frame: ToolDelta):
         super().__init__(frame)
         self.ListenPreload(self.on_preload)
 
     def on_preload(self):
-        self.GetPluginAPI("聊天栏菜单").add_trigger(
-            ["喇叭"], "[消息]", "管理广播消息", self.onLaba, op_only=True
+        # TODO: 目前无法在消息里使用空格
+        self.GetPluginAPI("聊天栏菜单").add_new_trigger(
+            ["喇叭"], [("消息", str, None)], "管理广播消息", self.onLaba, op_only=True
         )
 
-    def onLaba(self, playername: str, args: list[str]):
+    def onLaba(self, player: Player, args: tuple):
         msg = " ".join(args)
-        player = self.frame.get_players().getPlayerByName(playername)
-        if player is None:
-            raise ValueError("玩家不存在")
-        if msg.startswith(".喇叭"):
-            if player.is_op():
-                player.show(
-                    f"§l§b{player.name} §r§7>>> §l§b{msg.replace('.喇叭', '').strip()}",
-                )
-                self.game_ctrl.player_title("@a", f"§l§b{player.name}§f:")
-                self.game_ctrl.player_subtitle(
-                    "@a", f"§l§e{msg.replace('.喇叭', '').strip()}"
-                )
-                self.game_ctrl.sendwocmd(
-                    "execute as @a run playsound firework.launch @s ~~~ 10"
-                )
+        if player.is_op():
+            player.show(
+                f"§l§b{player.name} §r§7>>> §l§b{msg.replace('.喇叭', '').strip()}",
+            )
+            self.game_ctrl.player_title("@a", f"§l§b{player.name}§f:")
+            self.game_ctrl.player_subtitle(
+                "@a", f"§l§e{msg.replace('.喇叭', '').strip()}"
+            )
+            self.game_ctrl.sendwocmd(
+                "execute as @a run playsound firework.launch @s ~~~ 10"
+            )
+        else:
+            player.show("§c只有管理员可使用全服喇叭")
 
 
 entry = plugin_entry(全服喇叭)
