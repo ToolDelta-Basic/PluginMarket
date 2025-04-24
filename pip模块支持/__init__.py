b78ecb461378f0bdfc9a5c936377deaa2b7c750d
SuperScript-PRC <2528622340@qq.com>
2025-04-25 01:28:16 +0800

    pip 模块支持: 区分 pip 名与导入名

--- pip模块支持/__init__.py
+++ pip模块支持/__init__.py
@@ -7,7 +7,7 @@ from tooldelta import Plugin, utils, fmts, plugin_entry
 class PipSupport(Plugin):
     name = "pip模块安装支持"
     author = "ToolDelta"
-    version = (0, 0, 1)
+    version = (0, 0, 2)
 
     def __init__(self, frame):
         super().__init__(frame)
@@ -33,14 +33,34 @@ class PipSupport(Plugin):
         importlib.invalidate_caches()
         fmts.print_suc(f"模块 {', '.join(packages)} 安装成功")
 
-    def require(self, packages: str | list[str]):
-        if isinstance(packages, str):
-            packages = [packages]
-        for package in packages:
+    def require(self, module_pip_name_and_imp_name: dict[str, str] | str | list[str]):
+        """
+        显示需求一个 Python 模块。
+
+        Args:
+            package_pip_name_and_module_name (dict[str, str] | str): 模块的 pip 名与导入名
+        """
+        need_installed: list[str] = []
+        if isinstance(module_pip_name_and_imp_name, list):
+            # 多个库, 且库名与导入模块名相同
+            # 向下兼容
+            module_pip_name_and_imp_name = {
+                module_pip_name_and_imp_name: module_pip_name_and_imp_name
+                for module_pip_name_and_imp_name in module_pip_name_and_imp_name
+            }
+        if isinstance(module_pip_name_and_imp_name, str):
+            # 单个库, 且库名与导入模块名相同
+            # 向下兼容
+            module_pip_name_and_imp_name = {
+                module_pip_name_and_imp_name: module_pip_name_and_imp_name
+            }
+        for package_name, module_name in module_pip_name_and_imp_name.items():
             try:
-                importlib.import_module(package)
+                importlib.import_module(module_name)
             except ImportError:
-                self.install([package])
+                need_installed.append(package_name)
+        if need_installed:
+            self.install(need_installed)
 
     # -----------------------------------------------------------
     def on_console_pip(self, args: list[str]):
