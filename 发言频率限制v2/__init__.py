0c3968f90041537a2df7ec4cef4bec15e5c39e2b
SuperScript-PRC <2528622340@qq.com>
2025-04-28 20:25:42 +0800

    发言频率限制V2: feat: 自定义违规发言检测函数

--- 发言频率限制v2/__init__.py
+++ 发言频率限制v2/__init__.py
@@ -1,12 +1,12 @@
-from tooldelta import Plugin, ToolDelta, cfg, plugin_entry, utils, Chat, Player
+from tooldelta import Plugin, fmts, cfg, plugin_entry, utils, Chat, Player
 
 
 class AntiTooFastMessage_V2(Plugin):
     name = "发言频率限制v2"
     author = "SuperScript"
-    version = (0, 0, 8)
+    version = (0, 1, 0)
 
-    def __init__(self, frame: ToolDelta):
+    def __init__(self, frame):
         super().__init__(frame)
         CFG_DEFAULT = {
             "检测周期(秒)": 5,
@@ -17,16 +17,31 @@ class AntiTooFastMessage_V2(Plugin):
             ],
             "多少个换行判定为刷屏": 6,
             "多长的消息判定为刷屏": 100,
+            cfg.KeyGroup(
+                funckey := "自定义踢出逻辑lambda函数(玩家名:str,消息:str)->(违规消息:str,为空则视为不违规)"
+            ): "lambda playername,msg:''",
         }
 
         config, _ = cfg.get_plugin_config_and_version(
             "发言频率限制", cfg.auto_to_std(CFG_DEFAULT), CFG_DEFAULT, (0, 0, 5)
         )
+        if funckey not in config.keys():
+            config[funckey] = "lambda playername,msg:False"
+            cfg.upgrade_plugin_config("发言频率限制", config, self.version)
+            fmts.print_inf("发言频率限制 插件配置文件已更新")
         self.detect_time = config["检测周期(秒)"]
         self.msg_lmt = config["检测周期内最多发送多少条消息"]
         self.msg_lmt_anti = config["发言太快反制措施"]
         self.msg_length_limit = config["多长的消息判定为刷屏"]
         self.msg_lines_limit = config["多少个换行判定为刷屏"]
+        try:
+            self.invalid_msg_checker = eval(config[funckey])
+        except Exception as e:
+            fmts.print_err(f"{self.name} 插件配置文件自定义踢出逻辑函数格式错误: {e}")
+            raise SystemExit
+        if not callable(self.invalid_msg_checker):
+            fmts.print_err(f"{self.name} 插件配置文件自定义踢出逻辑函数: 不是一个函数对象")
+            raise SystemExit
         self.last_msgs: dict[str, int] = {}
 
         self.ListenActive(self.on_active)
@@ -52,9 +67,14 @@ class AntiTooFastMessage_V2(Plugin):
         self.last_msgs.setdefault(player.name, 0)
         self.last_msgs[player.name] += 1
         if player.is_op():
-           return
+            return
 
-        if len(msg) > self.msg_length_limit:
+        if display_str := self.invalid_msg_checker(player.name, msg):
+            self.game_ctrl.sendwocmd(
+                f"kick {player.xuid} {display_str}"
+            )
+            self.print(f"§6玩家 {player.name} 被违规消息检测函数检测到: {display_str}")
+        elif len(msg) > self.msg_length_limit:
             self.game_ctrl.sendwocmd(
                 f"kick {player.xuid} §c发言长度太长， 您已被踢出租赁服"
             )
