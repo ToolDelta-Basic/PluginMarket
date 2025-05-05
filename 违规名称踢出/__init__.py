fa8a6bb74d1752b5d8398f61aa20ddd7a199a4a2
SuperScript-PRC <2528622340@qq.com>
2025-05-06 00:25:15 +0800

    违规名称踢出: fix: CFG_STD

--- 违规名称踢出/__init__.py
+++ 违规名称踢出/__init__.py
@@ -10,7 +10,7 @@ from tooldelta import (
 
 
 class kill(Plugin):
-    version = (0, 0, 7)
+    version = (0, 0, 8)
     name = "违规名称踢出"
     author = "大庆油田"
     description = "简单的违规名称踢出"
@@ -19,12 +19,16 @@ class kill(Plugin):
         self.frame = frame
         self.game_ctrl = frame.get_game_control()
         self.make_data_path()
+        CFG_STD = {
+            cfg.KeyGroup("包含以下字样的玩家将被踢出"): cfg.JsonList(str),
+            "原因": "",
+        }
         CFG_DEFAULT = {
-            cfg.KeyGroup("包含以下字样的玩家将被踢出"): ["木合塔尔", "NO"],
+            "包含以下字样的玩家将被踢出": ["木合塔尔", "NO"],
             "原因": "",
         }
         self.cfg, _ = cfg.get_plugin_config_and_version(
-            self.name, {}, CFG_DEFAULT, self.version
+            self.name, CFG_STD, CFG_DEFAULT, self.version
         )
         self.ci = self.cfg.get("包含以下字样的玩家将被踢出", {})
         self.ci.extend(self.cfg.get("踢出词", {}))
