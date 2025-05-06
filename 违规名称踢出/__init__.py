4395bde92c1945428709a23e9246e39100121960
daqyt <daqyt@tooldelta.top>
2025-05-06 05:46:17 +0000

    违禁名称增加云词库

--- 违规名称踢出/__init__.py
+++ 违规名称踢出/__init__.py
@@ -7,10 +7,11 @@ from tooldelta import (
     Player,
     plugin_entry,
 )
-
-
+import requests
+from tooldelta.plugin_market import url_join
+from tooldelta.utils.tempjson import load_and_read
 class kill(Plugin):
-    version = (0, 0, 8)
+    version = (0, 0, 9)
     name = "违规名称踢出"
     author = "大庆油田"
     description = "简单的违规名称踢出"
@@ -22,16 +23,30 @@ class kill(Plugin):
         CFG_STD = {
             cfg.KeyGroup("包含以下字样的玩家将被踢出"): cfg.JsonList(str),
             "原因": str,
+            "是否使用云词库":bool
         }
         CFG_DEFAULT = {
             "包含以下字样的玩家将被踢出": ["木合塔尔", "NO"],
             "原因": "",
+            "是否使用云词库":True
         }
-        self.cfg, _ = cfg.get_plugin_config_and_version(
+        self.cfg, V = cfg.get_plugin_config_and_version(
             self.name, CFG_STD, CFG_DEFAULT, self.version
         )
-        self.ci = self.cfg.get("包含以下字样的玩家将被踢出", {})
-        self.ci.extend(self.cfg.get("踢出词", {}))
+        self.ci = self.cfg.get("包含以下字样的玩家将被踢出", [])
+        self.ci.extend(self.cfg.get("踢出词", []))
+        if self.cfg.get("是否使用云词库",True):
+            self.print("开始获取远程违禁名称")
+            try:
+                url = cfg.get_cfg(
+                "ToolDelta基本配置.json", {"插件市场源": str}
+            )["插件市场源"]
+                url = url_join(url,"违规名称踢出/词库.json")
+                r = requests.get(url).json()
+            except:
+                print("获取远程词库失败，将尝试使用本地词库")
+                r= load_and_read("插件文件/ToolDelta类式插件/违规名称踢出/词库.json",True,1,[])
+        self.ci.extend(r)
         self.yy = self.cfg["原因"]
         self.ListenPacket(constants.PacketIDS.PlayerList, self.on_prejoin)
         self.ListenActive(self.on_active)
