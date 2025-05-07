d5eca99db0023a45639f6a6586df94c19b3b45e6
Happy2018new <chenbo79800@163.com>
2025-05-07 19:14:48 +0800

    巡逻: 保证最小巡逻的时间间隔为 5 秒，并且默认配置改为 15 秒
    (妈的傻逼)

--- 巡逻/__init__.py
+++ 巡逻/__init__.py
@@ -5,21 +5,28 @@ import time
 class xunluo(Plugin):
     name = "巡逻"
     author = "猫七街"
-    version = (0, 0, 7)
+    version = (0, 0, 8)
 
     def __init__(self, frame):
         super().__init__(frame)
         self._default_cfg = {
-            "间隔时间（秒）": 0.1,
+            "间隔时间（秒）": 15,
         }
         self._std_cfg = {"间隔时间（秒）": float}
         try:
             self._cfg, _ = cfg.get_plugin_config_and_version(
                 self.name, self._std_cfg, self._default_cfg, self.version
             )
+            self.repeat_time = float(self._cfg["间隔时间（秒）"])
         except Exception as e:
             fmts.print_err(f"加载配置文件出错: {e}")
             self._cfg = self._default_cfg.copy()
+
+        if self.repeat_time < 5:
+            raise Exception(
+                f"巡逻: 最小时间间隔为 5 秒，但实际得到 {self.repeat_time} 秒，请修改配置文件！"
+            )
+        self.repeat_time = max(5, self.repeat_time)
 
         # 屏蔽 TextType:10 的刷屏。
         def on_filter(packet) -> bool:
@@ -44,7 +51,7 @@ class xunluo(Plugin):
                     self.game_ctrl.sendwocmd(
                         f'execute as @a[name="{player}"] at @s run tp @a[name="{self.game_ctrl.bot_name}"] ~ 325 ~'
                     )
-                    time.sleep(self._cfg["间隔时间（秒）"])
+                    time.sleep(self.repeat_time)
             except Exception as e:
                 fmts.print_err(f"巡逻出错: {e}")
 
