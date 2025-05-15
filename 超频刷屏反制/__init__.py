d0556b3b9fa8964783ed9f6bd8a85cc3fd62a2ed
SuperScript-PRC <2528622340@qq.com>
2025-05-15 19:39:25 +0800

    超频刷屏反制: 兼容最新封禁系统

--- 超频刷屏反制/__init__.py
+++ 超频刷屏反制/__init__.py
@@ -1,14 +1,12 @@
-from tooldelta import cfg as config, utils, ToolDelta, plugin_entry, Plugin, Player
-from tooldelta.constants import PacketIDS
-from tooldelta.game_utils import get_all_player
+from tooldelta import cfg as config, utils, Plugin, Player, Chat, TYPE_CHECKING, plugin_entry
 
 
 class HighRateChatAnti(Plugin):
     name = "超频发言反制"
     author = "SuperScript"
-    version = (0, 0, 4)
+    version = (0, 0, 5)
 
-    def __init__(self, frame: ToolDelta):
+    def __init__(self, frame):
         super().__init__(frame)
         CFG_DEFAULT = {
             "检测周期(秒)": 5,
@@ -20,14 +18,17 @@ class HighRateChatAnti(Plugin):
         )
         self.detect_time = cfg["检测周期(秒)"]
         self.msg_lmt = cfg["检测周期内最多发送多少条消息"]
-        self.ban_days = cfg["反制措施"]["封禁时间(天数)"] * 86400
+        self.ban_time = cfg["反制措施"]["封禁时间(天数)"] * 86400
         self.ListenPreload(self.on_def)
         self.ListenActive(self.on_inject)
-        self.ListenPacket(PacketIDS.Text, self.on_text_packet)
-        self.last_msgs: dict[str, int] = {}
+        self.ListenChat(self.on_chat)
+        self.last_msgs: dict[Player, int] = {}
 
     def on_def(self):
         self.ban = self.GetPluginAPI("封禁系统")
+        if TYPE_CHECKING:
+            from 封禁系统 import BanSystem
+            self.ban: BanSystem
 
     def on_inject(self):
         @utils.timer_event(self.detect_time, "重置刷屏检测时长")
@@ -36,27 +37,14 @@ class HighRateChatAnti(Plugin):
 
         clear_message_lmt()
 
-    def on_text_packet(self, pk: dict):
-        if pk["TextType"] in (1, 7):
-            if pk["SourceName"] == "":
-                try:
-                    self.on_chat(pk["Message"].split()[1])
-                except IndexError:
-                    self.print("[WARN] 无法从当前发言获取发送者")
-            else:
-                self.on_chat(pk["SourceName"])
-        return False
+    def on_chat(self, chat: Chat):
+        player = chat.player
+        self.last_msgs.setdefault(player, 0)
+        self.last_msgs[player] += 1
+        if self.is_too_fast(player):
+            self.ban.ban(player, self.ban_time, "超频刷屏")
 
-    def on_chat(self, playername: str):
-        if playername not in get_all_player():
-            return
-
-        self.last_msgs.setdefault(playername, 0)
-        self.last_msgs[playername] += 1
-        if self.is_too_fast(playername):
-            self.ban.ban(playername, self.ban_days, "超频刷屏")
-
-    def is_too_fast(self, player: str) -> bool:
+    def is_too_fast(self, player: Player) -> bool:
         return self.last_msgs.get(player, 0) > self.msg_lmt
 
     def player_leave(self, player: Player):
