7ce444ec6896820ea0ff75ae42de3b68792abf76
SuperScript-PRC <2528622340@qq.com>
2025-06-27 23:50:39 +0800

    tps计算器: fix: 在 1.21 的 TickSync 被废弃

--- 前置_tps计算器/__init__.py
+++ 前置_tps计算器/__init__.py
@@ -5,14 +5,16 @@ from tooldelta import Plugin, constants, plugin_entry
 class TPSCalculator(Plugin):
     name = "前置-TPS计算器"
     author = "SuperScript"
-    version = (0, 0, 1)
-    lt = time.time()
-    lt1 = 0
-    _tps = 20
+    version = (0, 0, 2)
 
     def __init__(self, frame):
         super().__init__(frame)
-        self.ListenPacket(constants.PacketIDS.IDTickSync, self.listener)
+        self.ListenPacket(constants.PacketIDS.IDSetTime, self.listener)
+        self.last_time = time.time()
+        self.last_server_time = 0
+        self._tps = 20
+        self._real_tps = 20
+        self._warned = False
 
     def listener(self, pkt):
         self.calc_tps(pkt)
@@ -20,13 +22,22 @@ class TPSCalculator(Plugin):
 
     def calc_tps(self, pkt):
         ntime = time.time()
-        servert = pkt["ServerReceptionTimestamp"]
-        self._tps = min(20, (servert - self.lt1) / (ntime - self.lt))
-        self.lt = ntime
-        self.lt1 = servert
+        server_time = pkt["Time"]
+        server_time_delta = server_time - self.last_server_time
+        real_time_delta = ntime - self.last_time
+        if server_time_delta == 0:
+            if not self._warned:
+                self._warned = True
+                self.print("§6警告: 无法正确取得租赁服 tps。你需要在设置中将§e昼夜更替§6打开。")
+            return
+        self._real_tps = server_time_delta / real_time_delta
+        self._tps = min(20, server_time_delta / self._real_tps)
+        self.last_time = ntime
+        self.last_server_time = server_time
+
 
     def get_tps(self):
-        if time.time() - self.lt > 40:
+        if time.time() - self.last_time > 40:
             # server maybe dead
             return 0
         else:
