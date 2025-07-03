889c092f1a34153ef8fa9f7ae7a254a2e3eae685
SuperScript-PRC <2528622340@qq.com>
2025-07-03 20:11:13 +0800

    性能管理: 适配 1.1.0

--- 性能管理/__init__.py
+++ 性能管理/__init__.py
@@ -1,8 +1,17 @@
 import psutil
 import os
-from tooldelta import Plugin, utils, fmts, cfg as config, plugin_entry
+from tooldelta import Plugin, utils, fmts, cfg, plugin_entry
 
 BYTES2MB = 1048576
+
+
+def pretty_show_bytes(bytes: int):
+    if bytes < 1024:
+        return f"{bytes}B"
+    elif bytes < 1048576:
+        return f"{bytes / 1024:.1f}KB"
+    else:
+        return f"{bytes / 1048576:.1f}MB"
 
 
 class EmergencyMetaMana(Plugin):
@@ -16,14 +25,20 @@ class EmergencyMetaMana(Plugin):
             "当内存占用超过多少百分比时提示警告": 80,
             "当内存占用超过多少百分比时停机": 95,
         }
-        cfg, _ = config.get_plugin_config_and_version(
-            self.name, config.auto_to_std(CFG), CFG, self.version
+        config, _ = cfg.get_plugin_config_and_version(
+            self.name, cfg.auto_to_std(CFG), CFG, self.version
         )
-        self.mem_warn_percent = cfg["当内存占用超过多少百分比时提示警告"] / 100
-        self.mem_exit_percent = cfg["当内存占用超过多少百分比时停机"] / 100
+        self.mem_warn = config["当内存占用超过多少百分比时提示警告"] / 100
+        self.mem_exit = config["当内存占用超过多少百分比时停机"] / 100
         self.ListenPreload(self.on_def)
 
     def on_def(self):
+        announce_bar = self.GetPluginAPI("更好的公告栏", force=False)
+        if announce_bar is not None:
+            announce_bar.regist_formatter(
+                "[RAM]", lambda: pretty_show_bytes(psutil.virtual_memory().used)
+            )
+            fmts.print_suc("性能管理: 已将 2 个格式加入公告栏插件")
         self.memory_mana()
         self.warn_lck = False
         self.hi_used = 0
@@ -32,13 +47,13 @@ class EmergencyMetaMana(Plugin):
         )
         fmts.print_inf("在控制台输入 性能 以查看当前系统的性能.")
 
-    @utils.timer_event(20, "内存告急停机检测")
+    @utils.timer_event(60, "内存告急停机检测")
     def memory_mana(self):
         vm = psutil.virtual_memory()
-        if vm.percent < self.mem_warn_percent:
+        if vm.percent < self.mem_warn * 100:
             self.warn_lck = False
             self.hi_used = 0
-        elif vm.percent > self.mem_warn_percent and (
+        elif vm.percent > self.mem_warn and (
             not self.warn_lck or vm.percent > self.hi_used
         ):
             self.hi_used = vm.percent
@@ -46,13 +61,16 @@ class EmergencyMetaMana(Plugin):
             fmts.print_war(
                 f"系统可用内存告急({vm.available / BYTES2MB:.2f}MB/{vm.total / BYTES2MB:.2f}MB 可用, {vm.percent:.2f}%)"
             )
-        if vm.percent > self.mem_exit_percent:
+        if vm.percent > self.mem_exit:
             fmts.print_err(
                 f"已超过最大可用内存限额({vm.available / BYTES2MB:.2f}MB/{vm.total / BYTES2MB:.2f}MB 可用, {vm.percent:.2f}%), 系统将退出"
             )
-            for k, _ in utils.tempjson.get_tmps():
-                utils.tempjson.unload_to_path(k)
-            os._exit(0)
+            self.exit()
+
+    @utils.thread_func("强制停机", thread_level=utils.ToolDeltaThread.SYSTEM)
+    def exit(self):
+        self.frame.system_exit("性能管理系统强制关机")
+        os._exit(0)
 
     def chk_proc(self, args):
         vm = psutil.virtual_memory()
@@ -72,6 +90,9 @@ class EmergencyMetaMana(Plugin):
             fmts.print_inf(
                 " top 2: 查看ToolDelta中运行的正常线程的列表", need_log=False
             )
+            fmts.print_inf(
+                " top 3: 查看ToolDelta中运行的定时任务的列表", need_log=False
+            )
             return
         if args[0] == "1":
             fmts.print_inf(
@@ -79,7 +100,7 @@ class EmergencyMetaMana(Plugin):
             )
             fmts.print_inf(" " + "\n ".join(self.test_hc()), need_log=False)
         elif args[0] == "2":
-            if not hasattr(utils, "get_threads_list"):
+            if not hasattr(utils.tooldelta_thread, "get_threads_list"):
                 fmts.print_war("该功能现在不可用")
             else:
                 lst = utils.tooldelta_thread.get_threads_list()
@@ -90,6 +111,12 @@ class EmergencyMetaMana(Plugin):
                     fmts.print_inf(
                         f" - {thr.usage or thr.func.__name__}", need_log=False
                     )
+        elif args[0] == "3":
+            tabl = utils.timer_events.timer_events_table
+            for k, v in sorted(tabl.items(), key=lambda x: x[0]):
+                fmts.print_inf(f"§b每 {k} 秒执行一次的定时任务:")
+                for v1 in v:
+                    fmts.print_inf(f"  {v1[0]}")
 
     def test_hc(self):
         return list(utils.tempjson.get_tmps().keys())
