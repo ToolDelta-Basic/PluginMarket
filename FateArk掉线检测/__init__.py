0f744a3a2bf27750efe4a25ca9c33603698a32d8
渊思 <zhonger-yuansi@outlook.com>
2025-08-02 22:58:28 +0800

    Updata FateArk掉线检测 (#126)
    * Updata FateArk掉线检测
    
    * 使用ToolDelta线程

---
+++ FateArk掉线检测/__init__.py
@@ -0,0 +1,35 @@
+from tooldelta import Plugin, plugin_entry, Player, Chat, FrameExit, game_utils, utils, fmts
+import time
+
+class NewPlugin(Plugin):
+    name = "FateArk掉线检测"
+    author = "渊思"
+    version = (0, 0, 1)
+
+    def __init__(self, frame):
+        super().__init__(frame)
+        self.ListenActive(self.Start)
+
+    def Start(self):
+        
+        utils.createThread(self.Check, (), "Fateark掉线检测")
+
+
+    def Check(self):
+        while True:
+            try:
+                res = self.game_ctrl.sendwscmd("testfor @a", waitForResp=True, timeout=10)
+                time.sleep(8)
+            except TimeoutError:
+                fmts.print_war("指令检测超时，正在进行二次检测")
+                try:
+                    res = self.game_ctrl.sendwscmd("testfor @a", waitForResp=True, timeout=10)
+                except TimeoutError:
+                    fmts.print_war("指令检测超时，正在进行三次检测")
+                    try:
+                        res = self.game_ctrl.sendwscmd("testfor @a", waitForResp=True, timeout=10)
+                    except TimeoutError:
+                        fmts.print_war("FateArk接入点机器人已掉线")
+                        self.frame.system_exit("FateArk接入点机器人已掉线")
+
+entry = plugin_entry(NewPlugin)
