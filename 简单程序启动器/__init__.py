326d3962dcf6413305524053704b021727f3505a
Happy2018new <chenbo79800@163.com>
2025-08-10 13:21:23 +0800

    Provide a simple custom program runner

---
+++ 简单程序启动器/__init__.py
@@ -0,0 +1,18 @@
+import subprocess
+from tooldelta import Plugin, Frame, plugin_entry
+
+
+class SimpleProgramRunner(Plugin):
+    name = "简单程序启动器"
+    author = "Eternal Crystal"
+    version = (0, 0, 1)
+
+    def __init__(self, _: Frame):
+        self.on_def()
+
+    def on_def(self):
+        args: list[str] = ["your_program_path", "some_args"]
+        subprocess.Popen(args).wait()
+
+
+entry = plugin_entry(SimpleProgramRunner, "简单程序启动器")
