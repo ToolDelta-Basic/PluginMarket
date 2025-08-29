06da99e1193b82c8d5c767c0d857cfb9a63c184d
Happy2018new <chenbo79800@163.com>
2025-08-29 20:06:29 +0800

    minor fix

--- bdx导出器Pro/__init__.py
+++ bdx导出器Pro/__init__.py
@@ -7,10 +7,10 @@ from tooldelta import (
 class BDXExporter(Plugin):
     name = "bdx导出器Pro"
     author = "SuperScript"
-    version = (0, 2, 0)
+    version = (0, 2, 1)
 
     def __init__(self, _):
-        raise Exception("bdx导出器Pro: 此组件现已弃用, 请前往 [简单世界导出] 作为替代")
+        raise Exception("bdx导出器Pro: 此组件现已弃用, 请使用 [简单世界导出] 作为替代")
 
 
 entry = plugin_entry(BDXExporter)
