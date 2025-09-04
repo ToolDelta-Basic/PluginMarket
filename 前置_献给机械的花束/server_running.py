bb8573925151d6a4ab1ee279dde405044b9cff88
Happy2018new <chenbo79800@163.com>
2025-09-04 09:50:06 +0800

    ffm: Fix not delete command request callback (possible memory leak)

--- 前置_献给机械的花束/server_running.py
+++ 前置_献给机械的花束/server_running.py
@@ -98,7 +98,7 @@ class FlowersForMachineServerRunning:
 
     def _download_server(self, server_path: str) -> bool:
         file_binary = requests.get(
-            "https://gh-proxy.com/https://github.com/OmineDev/flowers-for-machines/releases/download/v1.4.1/standard-server_linux_amd64"
+            "https://gh-proxy.com/https://github.com/OmineDev/flowers-for-machines/releases/download/v1.4.2/standard-server_linux_amd64"
         )
 
         if not file_binary.ok:
